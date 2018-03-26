4bb8c4b633b41d63a9511fb62bdd963daab7d588
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-26 12:42:39 -0300

    adding titles to skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -4,7 +4,10 @@ class Skills extends Component {
   render() {
     return (
       <div id="Skills" className="section">
-        <p>SKILLS</p>
+        <header>
+          <h3>Ah, I See You're a Man of Culture As Well...</h3>
+          <h2>Skills</h2>
+        </header>
       </div>
     );
   }
