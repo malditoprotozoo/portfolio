318b20f938a573e870baa5f899df02f3cf57f6d4
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-26 13:33:25 -0300

    generating data and adding images

---
+++ src/constants/dataProject.js
@@ -0,0 +1,26 @@
+const dataProject = [
+  {
+  title: 'PicBeautifier 3000',
+  description: 'picBeautifier 3000 is a Javascript library that will facilitate image manipulation, so you can add beautiful effects with just a few lines of code.',
+  repository: 'https://github.com/malditoprotozoo/picBeautifier-3000',
+  screenshot: './assets/img/picBeautifier.png',
+  demo: 'https://malditoprotozoo.github.io/picBeautifier-3000/index.html'
+  },
+  {
+    title: 'Nerd',
+    description: 'Nerd Bookstore is a simulation of online bookstore, developed as a sinple page application using the library Sammy.js.',
+    repository: 'https://github.com/malditoprotozoo/nerd',
+    screenshot: './assets/img/nerd.png',
+    demo: 'https://malditoprotozoo.github.io/nerd'
+  },
+  {
+    title: 'Prompt Me',
+    description: 'Prompt Me is a website made with Tumblr API and Firebase. It shows prompts obtained from Tumblr based on keywords or randomly generated',
+    repository: 'https://github.com/malditoprotozoo/promptMe',
+    screenshot: './assets/img/promptMe.png',
+    demo: 'https://malditoprotozoo.github.io/promptMe/'
+  }
+]
+
+
+export default dataProject;
