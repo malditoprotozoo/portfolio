1fc85c6b05fdf5c59b3b2222467da950086c8edb
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-28 18:08:21 -0300

    adding tags to projects database

--- src/constants/dataProject.js
+++ src/constants/dataProject.js
@@ -4,28 +4,32 @@ const dataProject = [
   description: 'picBeautifier 3000 is a Javascript library that will facilitate image manipulation, so you can add beautiful effects with just a few lines of code.',
   repository: 'https://github.com/malditoprotozoo/picBeautifier-3000',
   preview: 'https://i.imgur.com/QtdL4cn.png',
-  demo: 'https://malditoprotozoo.github.io/picBeautifier-3000/index.html'
+  demo: 'https://malditoprotozoo.github.io/picBeautifier-3000/index.html',
+  tags: ['css', 'jquery', 'javascript', 'library']
   },
   {
     title: 'Nerd',
     description: 'Nerd Bookstore is a simulation of online bookstore, developed as a sinple page application using the library Sammy.js.',
     repository: 'https://github.com/malditoprotozoo/nerd',
     preview: 'https://i.imgur.com/S6CCWHl.png',
-    demo: 'https://malditoprotozoo.github.io/nerd'
+    demo: 'https://malditoprotozoo.github.io/nerd',
+    tags: ['sammyjs', 'single-page-app', 'javascript', 'jquery', 'routing']
   },
   {
     title: 'Prompt Me',
     description: 'Prompt Me is a website made with Tumblr API and Firebase. It shows prompts obtained from Tumblr based on keywords or randomly generated',
     repository: 'https://github.com/malditoprotozoo/promptMe',
     preview: 'https://i.imgur.com/FuBXN1V.png',
-    demo: 'https://malditoprotozoo.github.io/promptMe/'
+    demo: 'https://malditoprotozoo.github.io/promptMe/',
+    tags: ['tumblr-api', 'firebase', 'firebase-auth', 'firebase-database', 'jquery', 'single-page-app', 'javascript']
   },
   {
     title: 'Twitter Replica',
     description: 'Designing a very basic replica of Twitter, desktop only.',
     repository: 'https://github.com/malditoprotozoo/twitter-replica',
     preview: 'https://i.imgur.com/QzNf7Lw.png',
-    demo: 'https://malditoprotozoo.github.io/twitter-replica/'
+    demo: 'https://malditoprotozoo.github.io/twitter-replica/',
+    tags: ['html', 'css', 'css3', 'icomoon']
   }
 ]
 
