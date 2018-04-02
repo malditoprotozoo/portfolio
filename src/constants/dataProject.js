b18806d1e3ca7ab8275107885b7010efb932a57a
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-04-02 02:23:06 -0300

    adding more projects

--- src/constants/dataProject.js
+++ src/constants/dataProject.js
@@ -1,15 +1,23 @@
 const dataProject = [
   {
-  title: 'PicBeautifier 3000',
-  description: 'picBeautifier 3000 is a Javascript library that will facilitate image manipulation, so you can add beautiful effects with just a few lines of code.',
-  repository: 'https://github.com/malditoprotozoo/picBeautifier-3000',
-  preview: 'https://i.imgur.com/QtdL4cn.png',
-  demo: 'https://malditoprotozoo.github.io/picBeautifier-3000/index.html',
-  tags: ['css', 'jquery', 'javascript', 'library']
+    title: 'PicBeautifier 3000',
+    description: 'picBeautifier 3000 is a Javascript library that will facilitate image manipulation, so you can add beautiful effects with just a few lines of code. Collaborative Project.',
+    repository: 'https://github.com/malditoprotozoo/picBeautifier-3000',
+    preview: 'https://i.imgur.com/QtdL4cn.png',
+    demo: 'https://malditoprotozoo.github.io/picBeautifier-3000/index.html',
+    tags: ['css', 'jquery', 'javascript', 'library']
+  },
+  {
+    title: 'Stony Library',
+    description: 'Index of fanfic recommendations using React and a webscraper made with Javascript.',
+    repository: 'https://github.com/malditoprotozoo/stonylibrary',
+    preview: 'https://i.imgur.com/PXA4GRw.png',
+    demo: 'https://malditoprotozoo.github.io/stonylibrary/',
+    tags: ['react', 'html', 'javascript', 'css', 'bootstrap', 'cheerio', 'request', 'web-scraper']
   },
   {
     title: 'Nerd',
-    description: 'Nerd Bookstore is a simulation of online bookstore, developed as a sinple page application using the library Sammy.js.',
+    description: 'Nerd Bookstore is a simulation of online bookstore, developed as a sinple page application using the library Sammy.js. Collaborative Project.',
     repository: 'https://github.com/malditoprotozoo/nerd',
     preview: 'https://i.imgur.com/S6CCWHl.png',
     demo: 'https://malditoprotozoo.github.io/nerd',
@@ -17,7 +25,7 @@ const dataProject = [
   },
   {
     title: 'Prompt Me',
-    description: 'Prompt Me is a website made with Tumblr API and Firebase. It shows prompts obtained from Tumblr based on keywords or randomly generated',
+    description: 'Prompt Me is a website made with Tumblr API and Firebase. It shows prompts obtained from Tumblr based on keywords or randomly generated. Collaborative Project.',
     repository: 'https://github.com/malditoprotozoo/promptMe',
     preview: 'https://i.imgur.com/FuBXN1V.png',
     demo: 'https://malditoprotozoo.github.io/promptMe/',
@@ -29,11 +37,19 @@ const dataProject = [
     repository: 'https://github.com/malditoprotozoo/twitter-replica',
     preview: 'https://i.imgur.com/QzNf7Lw.png',
     demo: 'https://malditoprotozoo.github.io/twitter-replica/',
-    tags: ['html', 'css', 'css3', 'icomoon']
+    tags: ['html', 'css', 'css3', 'icomoon', 'javascript', 'dom']
+  },
+  {
+    title: 'Marveldex',
+    description: 'Website about the Marvel characters using Marvel\'s official API and React. Collaborative Project.',
+    repository: 'https://github.com/malditoprotozoo/marveldex',
+    preview: 'https://i.imgur.com/MigbKxc.png',
+    demo: 'https://malditoprotozoo.github.io/marveldex/',
+    tags: ['html', 'css', 'css3', 'api', 'jsx', 'react', 'marvel-api', 'bootstrap-react']
   },
   {
     title: 'Marvel Movieverse',
-    description: 'Website about the Marvel movies and its comic books made for Laboratoria\'s first hackathon.',
+    description: 'Website about the Marvel movies and its comic books made for Laboratoria\'s first hackathon. Collaborative Project.',
     repository: 'https://github.com/malditoprotozoo/marvel-movieverse',
     preview: 'https://i.imgur.com/nUoe6Tp.png',
     demo: 'https://malditoprotozoo.github.io/marvel-movieverse/',
